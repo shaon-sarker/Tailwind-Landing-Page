564da8512b1deba6d6448389a9d3596f0a909188
shaon-sarker <60789407+shaon-sarker@users.noreply.github.com>
2021-06-14 13:15:27 +0600

    jquery others update

---
+++ js/main.js
@@ -0,0 +1,30 @@
+AOS.init({
+    duration: 1000
+  });
+
+$('.js-preloader').preloadinator({
+    minTime: 2000
+});
+
+$("body").toTopButton({
+
+  // path to icons
+  imagePath: 'images/icons',
+
+  // arrow, arrow-circle, caret, caret-circle, circle, circle-o, arrow-l, drop, rise, top
+  // or your own SVG icon
+  arrowType: 'arrow',
+
+  // 'w' = white
+  // 'b' = black
+  iconColor: 'w',
+
+  // icon shadow
+  // from 1 to 16
+  iconShadow: 4
+
+
+
+
+
+});
